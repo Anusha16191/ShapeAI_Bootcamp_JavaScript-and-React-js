e4593fc9cbb44b494addb6a0188d20e0fcb277f6
Anusha16191 <84612177+Anusha16191@users.noreply.github.com>
2021-06-05 19:54:18 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>copyright @ 2021</h3>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3>JS And React BootCamp</h3>
+      <p>It was very usefull for me.</p>
+    </div>
+  );
+}
+
+export default Note;
